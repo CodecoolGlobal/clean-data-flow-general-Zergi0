6c6c17d6b20e1110ca907ebf935401526f4e6541
Zergi <bestestyasuo@gmail.com>
2024-08-30 15:39:35 +0200

    added LoginPage class

--- testing/src/test/java/LoginPage.java
+++ testing/src/test/java/LoginPage.java
@@ -1,2 +1,38 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class LoginPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(css = "html > body > div:nth-of-type(2) > div")
+    private WebElement formContainer;
+
+    @FindBy(id = "user")
+    private WebElement username;
+
+    @FindBy(id = "inputPassword")
+    private WebElement password;
+
+    @FindBy(css = "button[class*='btn-primary']")
+    private WebElement loginButton;
+
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver,this);
+    }
+
+    public void login(String userNameString, String passwordString) {
+        wait.until(ExpectedConditions.visibilityOf(formContainer));
+        username.clear();
+        username.sendKeys(userNameString);
+        password.clear();
+        password.sendKeys(passwordString);
+        loginButton.click();
+    }
 }
