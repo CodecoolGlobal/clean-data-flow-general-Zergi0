cdceffcaf127dc88a1699b5fbe2afc2d03d38de9
Zergi <bestestyasuo@gmail.com>
2024-11-07 11:24:42 +0100

    added environment variables and replaced hard coded testing cases

--- testing/src/test/java/NewDataTests.java
+++ testing/src/test/java/NewDataTests.java
@@ -21,6 +21,9 @@ public class NewDataTests {
     private Navbar navbar;
     private DataEntryPage dataEntryPage;
 
+    private String userName;
+    private String password;
+
     @BeforeEach
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
@@ -34,6 +37,9 @@ public class NewDataTests {
         registerPage = new RegisterPage(driver, wait);
         loginPage = new LoginPage(driver, wait);
         navbar = new Navbar(driver, wait);
+
+        userName = System.getenv("TEST_USER_NAME");
+        password = System.getenv("TEST_USER_PASSWORD");
         dataEntryPage = new DataEntryPage(driver, wait);
 
         driver.get("http://localhost:5555/");
@@ -43,9 +49,9 @@ public class NewDataTests {
     @ValueSource(strings = {"000001AA", "999999AA", "123456AA", "000000AA", "123456XX", "123456ZZ"})
     public void idCardTestCorrectBoundaries(String idCardNumber) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", idCardNumber, "2-19870423-0726",
                 "test@test.test", "12345678", "test 1", "Budapest", "Budapest",
@@ -57,9 +63,9 @@ public class NewDataTests {
     @ValueSource(strings = {"123456asd", "12345asd", "981asdmkd"})
     public void idCardTestIncorrectBoundaries(String idCardNumber) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", idCardNumber, "2-19870423-0726",
                 "test@test.test", "12345678", "test 1", "Budapest", "Budapest",
@@ -71,9 +77,9 @@ public class NewDataTests {
     @ValueSource(ints = {1234, 9999, 1000, 1111})
     public void ZipCodeCorrectBoundaries(int zip) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", "123456AA", "2-19870423-0726",
                 "test@test.test", "12345678", "test 1", "Budapest", "Budapest",
@@ -85,9 +91,9 @@ public class NewDataTests {
     @ValueSource(ints = {19999, 12333, 9999999, 1, 22, 33})
     public void ZipCodeIncorrectBoundaries(int zip) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", "123456AA", "2-19870423-0726",
                 "test@test.test", "12345678", "test 1", "Budapest", "Budapest",
@@ -99,9 +105,9 @@ public class NewDataTests {
     @ValueSource(strings = {"12345678", "11111111", "11111111111", "99999999", "99999999999"})
     public void PhoneNumberCorrectBoundaries(String phoneNumber) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", "123456AA", "2-19870423-0726",
                 "test@test.test", phoneNumber, "test 1", "Budapest", "Budapest",
@@ -113,9 +119,9 @@ public class NewDataTests {
     @ValueSource(strings = {"1111111", "99999999999999999999"})
     public void PhoneNumberIncorrectBoundaries(String phoneNumber) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", "123456AA", "2-19870423-0726",
                 "test@test.test", phoneNumber, "test 1", "Budapest", "Budapest",
@@ -127,9 +133,9 @@ public class NewDataTests {
     @ValueSource(strings = {"2-19870423-0726"})
     public void PersonalCorrectBoundaries(String idNumber) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", "123456AA", idNumber,
                 "test@test.test", "12345678", "test 1", "Budapest", "Budapest",
@@ -141,9 +147,9 @@ public class NewDataTests {
     @ValueSource(strings = {"2-19870423-0701", "2-19870423-0700"})
     public void PersonalIncorrectBoundaries(String idNumber) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", "123456AA", idNumber,
                 "test@test.test", "12345678", "test 1", "Budapest", "Budapest",
@@ -155,9 +161,9 @@ public class NewDataTests {
     @ValueSource(strings = {"20220101", "20271231", "20251222"})
     public void DateCorrectBoundaries(String date) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", "123456AA", "2-19870423-0726",
                 "test@test.test", "12345678", "test 1", "Budapest", "Budapest",
@@ -169,9 +175,9 @@ public class NewDataTests {
     @ValueSource(strings = {"20211231", "20290101"})
     public void DateIncorrectBoundaries(String date) {
         loggedOutNavbar.clickNavbarRegister();
-        registerPage.register("testtest", "Testtest1");
+        registerPage.register(userName, password);
         loggedOutNavbar.clickNavbarLogin();
-        loginPage.login("testtest", "Testtest1");
+        loginPage.login(userName, password);
         navbar.clickDataEntryPage();
         boolean pass = dataEntryPage.addNewEntry("test", "test", "123456AA", "2-19870423-0726",
                 "test@test.test", "12345678", "test 1", "Budapest", "Budapest",
